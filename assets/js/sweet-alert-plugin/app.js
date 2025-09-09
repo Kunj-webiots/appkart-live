d5e6d02b55ee42e4a0aa4b63eb8a1e19bfa1e2b3
shraddha <shraddha215webiots@gmail.com>
2025-09-09 10:12:02 +0530

    added

---
+++ assets/js/sweet-alert-plugin/app.js
@@ -0,0 +1,28 @@
+// Sweet alert JS
+
+(function () {
+  var SweetAlert_custom = {
+    init: function () {
+      document.querySelector(".sweet-15").onclick = async function () {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 2000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          },
+        });
+        Toast.fire({
+          title: 'Success!',
+          icon: "success",
+          title: "Saved successfully.",
+        });
+      };
+    },
+  };
+
+  SweetAlert_custom.init();
+})();
---
+++ assets/js/text-type.js
@@ -0,0 +1,11 @@
+/* ==============
+1.auto text change JS
+==================*/
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('.auto-type', {
+        strings: ['City', 'Railway station', 'Mall', 'Bus station', 'Sport stadium', 'thank you'],
+        typeSpeed: 130,
+        backSpeed: 130,
+        loop: true,
+    });
+});
